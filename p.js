acd2e25578ab9ec26d84e1b71aeef09cafaa3cea
Inaciope <52049570+Inaciope@users.noreply.github.com>
2019-06-20 17:49:21 -0300

    Create p.js

---
+++ p.js
@@ -0,0 +1,56 @@
+/ 1° /
+//
+function Areacirculo(raio) 
+{
+    return 2 * Math.PI * raio
+}
+console.log(Areacirculo( ! )
+_______________________________________________________________________________________________________________________
+/ 2° /
+//
+function Areatriangulo(base, altura) 
+{
+    return (base * altura) / 2
+}
+console.log(Areatriangulo(! , !))
+_______________________________________________________________________________________________________________________
+/ 3° /
+//
+function Areaquadrado(l) 
+{
+    return l ** 2
+}
+console.log(Areaquadrado( ! ))
+_______________________________________________________________________________________________________________________
+/ 4° /
+//
+function Arearetangulo(base, altura) 
+{
+    if (base != altura)
+    {
+        return base * altura    
+    }
+}
+console.log(Arearetangulo(! , ! ))
+_______________________________________________________________________________________________________________________
+/ 5° /
+//
+function Areatrapezio(basemenor, basemaior, altura) 
+{
+    if (basemaior != basemenor) 
+    {
+    return ((basemaior + basemenor) * altura) / 2
+    }    
+}
+console.log(Areatrapezio(! , ! , ! ))
+_______________________________________________________________________________________________________________________
+ / 6° /
+//
+ function Arealosangulo(diagonalmaior, diagonalmenor) 
+{
+    if(diagonalmaior != diagonalmenor)
+    {
+        return (diagonalmaior * diagonalmenor) / 2
+    }    
+}
+console.log(Arealosangulo(! , ! ))
